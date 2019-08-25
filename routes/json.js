2ac837795f7f8a8c04fe3c77e9406393780fb99f
raghavaadi <aadithyaraghav@gmail.com>
2019-08-25 19:18:29 +0530

    -

--- routes/json.js
+++ routes/json.js
@@ -5,22 +5,22 @@ const router = express.Router();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-
-
-
-
-
-
 router.get('/',function(req,res,next){
 	res.render('json',{
 		title :'Kaliyuga Tamizha',
-		name : 'Raghav'
+		name : 'Raghav',
+		result: ''
 	});
 });
 router.post('/', urlencodedParser, function (req, res) {
-	console.log(req.body.post-text);
-	
-	 res.send('welcome, ' + req.body.post-text);
+	//console.log(req.body.post-text);
+	res=req.body.post-text;
+	console.log(JSON.stringify(res));
+	// res=req.body.post-text;
+	//  res.render('json', {
+	// 	title :'Kaliyuga Tamizha',
+	// 	name : 'Raghav',
+	// 	result: ''
+	// });
 })
 module.exports= router;
